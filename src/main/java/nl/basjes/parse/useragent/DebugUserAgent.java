9565cdebbb287d4cd96b230440d62ab823c97311
Niels Basjes <nbasjes@bol.com>
2016-08-07 17:10:23 +0200

    Detection for ambiguous matchers

--- src/main/java/nl/basjes/parse/useragent/DebugUserAgent.java
+++ src/main/java/nl/basjes/parse/useragent/DebugUserAgent.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DebugUserAgent extends UserAgent {
 
@@ -43,9 +45,38 @@ public class DebugUserAgent extends UserAgent {
                 }
             }
         }
-
         sb.append("+==========================================\n");
         return sb.toString();
+    }
+
+    public boolean analyzeMatchersResult() {
+        boolean passed = true;
+        for (String fieldName : getAvailableFieldNamesSorted()) {
+            String value = getValue(fieldName);
+            Map<Long, String> receivedValues = new HashMap<>(32);
+            for (UserAgent result: appliedMatcherResults) {
+                AgentField partialField = result.get(fieldName);
+                if (partialField != null && partialField.confidence >= 0) {
+                    String previousValue = receivedValues.get(partialField.confidence);
+                    if (previousValue != null) {
+                        if (!previousValue.equals(partialField.getValue())) {
+                            if (passed) {
+                                LOG.error("***********************************************************");
+                                LOG.error("***        REALLY IMPORTANT ERRORS IN THE RULESET       ***");
+                                LOG.error("*** YOU MUST CHANGE THE CONFIDENCE LEVELS OF YOUR RULES ***");
+                                LOG.error("***********************************************************");
+                            }
+                            passed = false;
+                            LOG.error("Found different value for \"{}\" with SAME confidence {}: \"{}\" and \"{}\"",
+                                fieldName, partialField.confidence, previousValue, partialField.getValue());
+                        }
+                    } else {
+                        receivedValues.put(partialField.confidence, partialField.getValue());
+                    }
+                }
+            }
+        }
+        return passed;
     }
 
 
