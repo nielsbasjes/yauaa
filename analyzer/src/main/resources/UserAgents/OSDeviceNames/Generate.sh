f319bf6b752c53dd58a8eba8aeb3f6aa3f594b8a
Niels Basjes <nbasjes@bol.com>
2016-10-25 13:55:49 +0200

    Changed license from GPL to Apache 2.0

--- analyzer/src/main/resources/UserAgents/OSDeviceNames/Generate.sh
+++ analyzer/src/main/resources/UserAgents/OSDeviceNames/Generate.sh
@@ -2,40 +2,38 @@
 # Yet Another UserAgent Analyzer
 # Copyright (C) 2013-2016 Niels Basjes
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 (
 echo "# ============================================="
 echo "# THIS FILE WAS GENERATED; DO NOT EDIT MANUALLY"
 echo "# ============================================="
-
+echo "#"
 echo "# Yet Another UserAgent Analyzer"
 echo "# Copyright (C) 2013-2016 Niels Basjes"
 echo "#"
-echo "# This program is free software: you can redistribute it and/or modify"
-echo "# it under the terms of the GNU General Public License as published by"
-echo "# the Free Software Foundation, either version 3 of the License, or"
-echo "# (at your option) any later version."
+echo "# Licensed under the Apache License, Version 2.0 (the \"License\");"
+echo "# you may not use this file except in compliance with the License."
+echo "# You may obtain a copy of the License at"
 echo "#"
-echo "# This program is distributed in the hope that it will be useful,"
-echo "# but WITHOUT ANY WARRANTY; without even the implied warranty of"
-echo "# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
-echo "# GNU General Public License for more details."
+echo "# http://www.apache.org/licenses/LICENSE-2.0"
 echo "#"
-echo "# You should have received a copy of the GNU General Public License"
-echo "# along with this program.  If not, see <http://www.gnu.org/licenses/>."
-
+echo "# Unless required by applicable law or agreed to in writing, software"
+echo "# distributed under the License is distributed on an \"AS IS\" BASIS,"
+echo "# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied."
+echo "# See the License for the specific language governing permissions and"
+echo "# limitations under the License."
+echo "#"
 echo "config:"
 
 cat "OperatingSystemDeviceNames.csv" | grep . | fgrep -v '#' | while read line ; \
