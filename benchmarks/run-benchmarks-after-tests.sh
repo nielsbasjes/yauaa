cac0b4abe537bc3dabe183b04a6330114a97ff0a
Niels Basjes <niels@basjes.nl>
2017-11-25 10:35:50 +0100

    Small improvement benchmark script

--- benchmarks/run-benchmarks-after-tests.sh
+++ benchmarks/run-benchmarks-after-tests.sh
@@ -16,7 +16,7 @@
 # limitations under the License.
 #
 
-( cd .. && mvn clean package ) || exit
+( cd .. && mvn clean package -DskipTests=true && mvn test ) || exit
 version=$(fgrep '<version>' pom.xml | head -1 | sed 's@.*>\(.*\)<.*$@\1@g')
 echo "Testing version ${version}" && \
 java -jar target/benchmarks.jar > version-${version}-$(date +%Y%m%d-%H%M%S).txt
