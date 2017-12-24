c067d1880cae77116d0b6502796c2056e1d82026
Niels Basjes <niels@basjes.nl>
2017-12-24 15:57:49 +0100

    Fix warning

--- udfs/flink/src/test/java/nl/basjes/parse/useragent/flink/TestUserAgentAnalysisMapperRaw.java
+++ udfs/flink/src/test/java/nl/basjes/parse/useragent/flink/TestUserAgentAnalysisMapperRaw.java
@@ -43,7 +43,7 @@ public class TestUserAgentAnalysisMapperRaw {
     }
 
     @Test
-    public void testUserAgentParser() throws Exception {
+    public void testUserAgentParser() {
         TestMapper mapper = new TestMapper();
 
         mapper.open(null);
