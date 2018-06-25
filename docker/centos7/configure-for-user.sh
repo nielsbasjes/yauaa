914665bda1b619adbb93e344d1ab04aa367ed765
Niels Basjes <niels@basjes.nl>
2018-06-25 16:35:39 +0200

    Added shellcheck to docker image and fixed reported scripting problems

--- docker/centos7/configure-for-user.sh
+++ docker/centos7/configure-for-user.sh
@@ -23,18 +23,20 @@ USER_ID=$2
 GROUP_ID=$3
 
 
-groupadd --non-unique -g ${GROUP_ID} ${USER_NAME}
-useradd -g ${GROUP_ID} -u ${USER_ID} -k /root -m ${USER_NAME}
+groupadd --non-unique -g "${GROUP_ID}" "${USER_NAME}"
+useradd -g "${GROUP_ID}" -u "${USER_ID}" -k /root -m "${USER_NAME}"
 
-echo "export HOME=/home/${USER_NAME}" >> /home/${USER_NAME}/.bashrc
-echo "export USER=${USER_NAME}" >> /home/${USER_NAME}/.bashrc
-echo '. /scripts/env.sh' >> /home/${USER_NAME}/.bashrc
+{
+echo "export HOME=/home/${USER_NAME}"
+echo "export USER=${USER_NAME}"
+echo '. /scripts/env.sh'
+} >> "/home/${USER_NAME}/.bashrc"
 
 VBOXSF_GROUP_LINE=$4
-if [[ -n ${VBOXSF_GROUP_LINE} ]];
+if [[ -n "${VBOXSF_GROUP_LINE}" ]];
 then
-    echo ${VBOXSF_GROUP_LINE} >> /etc/group
-    usermod -aG vboxsf ${USER_NAME}
+    echo "${VBOXSF_GROUP_LINE}" >> /etc/group
+    usermod -aG vboxsf "${USER_NAME}"
 fi
 
-echo "${USER_NAME}    ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/${USER_NAME}
+echo "${USER_NAME}    ALL=(ALL) NOPASSWD: ALL" >> "/etc/sudoers.d/${USER_NAME}"
--- start-docker.sh
+++ start-docker.sh
@@ -47,31 +47,31 @@ ${DOCKER_BUILD} -t ${PROJECTNAME}-${OS} docker/${OS}
 
 if [ "$(uname -s)" == "Linux" ]; then
   USER_NAME=${SUDO_USER:=${USER}}
-  USER_ID=$(id -u ${USER_NAME})
-  GROUP_ID=$(id -g ${USER_NAME})
+  USER_ID=$(id -u "${USER_NAME}")
+  GROUP_ID=$(id -g "${USER_NAME}")
 else # boot2docker uid and gid
   USER_NAME=${USER}
   USER_ID=1000
   GROUP_ID=50
 fi
 
-${DOCKER_BUILD} -t ${PROJECTNAME}-${OS}-${USER_NAME} - <<UserSpecificDocker
+${DOCKER_BUILD} -t "${PROJECTNAME}-${OS}-${USER_NAME}" - <<UserSpecificDocker
 FROM ${PROJECTNAME}-${OS}
-RUN bash /scripts/configure-for-user.sh ${USER_NAME} ${USER_ID} ${GROUP_ID} "$(fgrep vboxsf /etc/group)"
+RUN bash /scripts/configure-for-user.sh "${USER_NAME}" "${USER_ID}" "${GROUP_ID}" "$(fgrep vboxsf /etc/group)"
 UserSpecificDocker
 
 # Do NOT Map the real ~/.m2 directory !!!
-[ -d ${PWD}/docker/_m2    ] || mkdir ${PWD}/docker/_m2
-[ -d ${PWD}/docker/_gnupg ] || mkdir ${PWD}/docker/_gnupg
+[ -d "${PWD}/docker/_m2"    ] || mkdir "${PWD}/docker/_m2"
+[ -d "${PWD}/docker/_gnupg" ] || mkdir "${PWD}/docker/_gnupg"
 
-docker run --rm=true -t -i                                    \
-           -u ${USER_NAME}                                    \
-           -v ${PWD}:/home/${USER_NAME}/${PROJECTNAME}        \
-           -v ${PWD}/docker/_m2:/home/${USER_NAME}/.m2        \
-           -v ${PWD}/docker/_gnupg:/home/${USER_NAME}/.gnupg  \
-           -w /home/${USER}/${PROJECTNAME}                    \
-           --name "${CONTAINER_NAME}"                         \
-           ${PROJECTNAME}-${OS}-${USER_NAME}                  \
+docker run --rm=true -t -i                                      \
+           -u "${USER_NAME}"                                    \
+           -v "${PWD}:/home/${USER_NAME}/${PROJECTNAME}"        \
+           -v "${PWD}/docker/_m2:/home/${USER_NAME}/.m2"        \
+           -v "${PWD}/docker/_gnupg:/home/${USER_NAME}/.gnupg"  \
+           -w "/home/${USER}/${PROJECTNAME}"                    \
+           --name "${CONTAINER_NAME}"                           \
+           "${PROJECTNAME}-${OS}-${USER_NAME}"                  \
            bash
 
 exit 0
