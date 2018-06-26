3e06894f7cd5eff5c6a2a55f4e84f354fad5e8b3
Niels Basjes <niels@basjes.nl>
2018-06-26 21:23:21 +0200

    Use newer API for unit testing Beam UDF

--- udfs/beam/src/test/java/nl/basjes/parse/useragent/beam/TestUserAgentAnalysisDoFnRaw.java
+++ udfs/beam/src/test/java/nl/basjes/parse/useragent/beam/TestUserAgentAnalysisDoFnRaw.java
@@ -18,18 +18,27 @@
 package nl.basjes.parse.useragent.beam;
 
 import nl.basjes.parse.useragent.annotate.YauaaField;
+import org.apache.beam.runners.direct.repackaged.com.google.common.util.concurrent.UncheckedExecutionException;
+import org.apache.beam.sdk.testing.NeedsRunner;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.testing.ValidatesRunner;
+import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.DoFnTester;
-import org.apache.beam.sdk.util.UserCodeException;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import java.io.Serializable;
-import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-
+@Category(ValidatesRunner.class)
 public class TestUserAgentAnalysisDoFnRaw implements Serializable {
+
+    @Rule
+    public final transient TestPipeline pipeline = TestPipeline.create();
+
     public static class TestDoFn extends UserAgentAnalysisDoFn<TestRecord> {
         @Override
         public String getUserAgentString(TestRecord record) {
@@ -48,24 +57,28 @@ public class TestUserAgentAnalysisDoFnRaw implements Serializable {
     }
 
     @Test
+    @Category(NeedsRunner.class)
     public void testUserAgentAnalysisDoFn() throws Exception {
-        DoFn<TestRecord, TestRecord> fn = new TestDoFn();
+        // Process a single input element:
+        String userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) " +
+            "Chrome/48.0.2564.82 Safari/537.36";
+        TestRecord testInput = new TestRecord(userAgent);
 
-        DoFnTester<TestRecord, TestRecord> fnTester = DoFnTester.of(fn);
+        TestRecord expectedOutput = new TestRecord(userAgent);
+        expectedOutput.agentNameVersion = "Chrome 48.0.2564.82";
+        expectedOutput.deviceClass = "Desktop";
 
-        // Process a bundle containing a single input element:
-        TestRecord testInput = new TestRecord("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) " +
-            "Chrome/48.0.2564.82 Safari/537.36");
-        List<TestRecord> testOutputs = fnTester.processBundle(testInput);
+        PCollection<TestRecord> input = pipeline
+            .apply("Create", Create.of(testInput));
 
-        assertEquals(1, testOutputs.size());
-        TestRecord record = testOutputs.get(0);
-        assertEquals("Desktop", record.deviceClass);
-        assertEquals("Chrome 48.0.2564.82", record.agentNameVersion);
-        assertNull(record.shouldRemainNull);
+        PCollection<TestRecord> testOutputs = input
+            .apply(ParDo.of(new TestDoFn()));
+
+        PAssert.that(testOutputs).containsInAnyOrder(expectedOutput);
+        pipeline.run();
     }
 
-    @Test(expected = UserCodeException.class)
+    @Test(expected = UncheckedExecutionException.class)
     public void testImpossibleField() throws Exception {
         DoFn<TestRecord, TestRecord> fn = new UserAgentAnalysisDoFn<TestRecord>() {
             @Override
@@ -79,11 +92,16 @@ public class TestUserAgentAnalysisDoFnRaw implements Serializable {
             }
         };
 
-        DoFnTester<TestRecord, TestRecord> fnTester = DoFnTester.of(fn);
+        String userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) " +
+            "Chrome/48.0.2564.82 Safari/537.36";
+        TestRecord testInput = new TestRecord(userAgent);
 
-        // Process a bundle containing a single input element:
-        TestRecord testInput = new TestRecord("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) " +
-            "Chrome/48.0.2564.82 Safari/537.36");
-        fnTester.processBundle(testInput);
+        PCollection<TestRecord> input = pipeline
+            .apply("Create", Create.of(testInput));
+
+        PCollection<TestRecord> testOutputs = input
+            .apply(ParDo.of(fn));
+
+        pipeline.run();
     }
 }
