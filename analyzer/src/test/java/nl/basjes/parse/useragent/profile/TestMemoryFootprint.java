75b61b167af8e6d5b743f157cfcd32a2145b96a4
Niels Basjes <niels@basjes.nl>
2018-06-28 13:22:26 +0200

    Checkstyle

--- analyzer/src/test/java/nl/basjes/parse/useragent/profile/TestMemoryFootprint.java
+++ analyzer/src/test/java/nl/basjes/parse/useragent/profile/TestMemoryFootprint.java
@@ -23,8 +23,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.Assert.assertTrue;
-
 public class TestMemoryFootprint {
 
     private static final Logger LOG = LoggerFactory.getLogger(TestMemoryFootprint.class);
