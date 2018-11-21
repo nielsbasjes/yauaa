5efa046ad692a7cde51a3d8645d932a3dea34802
Niels Basjes <niels@basjes.nl>
2018-11-21 12:14:23 +0100

    Refactor prefix finding using a Trie

--- analyzer/src/main/java/nl/basjes/parse/useragent/utils/PrefixLookup.java
+++ analyzer/src/main/java/nl/basjes/parse/useragent/utils/PrefixLookup.java
@@ -1,48 +1,100 @@
+/*
+ * Yet Another UserAgent Analyzer
+ * Copyright (C) 2013-2018 Niels Basjes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package nl.basjes.parse.useragent.utils;
 
-import org.apache.commons.lang3.tuple.Pair;
-
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 public class PrefixLookup implements Serializable {
 
-    private final List<Pair<String, String>> sortedPrefixList;
+    public static class PrefixTrie implements Serializable {
+        private PrefixTrie[] childNodes;
+        private int          charIndex;
+
+        private String theValue;
+
+        public PrefixTrie() {
+            this(0);
+        }
+
+        private PrefixTrie(int charIndex) {
+            this.charIndex = charIndex;
+        }
+
+        private void add(char[] prefix, String value) {
+            if (charIndex == prefix.length) {
+                theValue = value;
+                return;
+            }
+
+            int myChar = prefix[charIndex]; // This will give us the ASCII value of the char
+            if (myChar < 32 || myChar > 126) {
+                throw new IllegalArgumentException("Only readable ASCII is allowed as key !!!");
+            }
+
+            if (childNodes == null) {
+                childNodes = new PrefixTrie[128];
+            }
+
+            if (childNodes[myChar] == null) {
+                childNodes[myChar] = new PrefixTrie(charIndex+1);
+            }
+
+            childNodes[myChar].add(prefix, value);
+        }
+
+        public void add(String prefix, String value) {
+            add(prefix.toCharArray(), value);
+        }
+
+        public String find(char[] input) {
+            if (charIndex == input.length) {
+                return theValue;
+            }
+            int myChar = input[charIndex]; // This will give us the ASCII value of the char
+            if (myChar < 32 || myChar > 126) {
+                return null; // Cannot store these, so never an answer.
+            }
+
+            if (childNodes == null || childNodes[myChar] == null) {
+                return theValue;
+            }
+            String returnValue = childNodes[myChar].find(input);
+            if (returnValue == null) {
+                return theValue;
+            }
+            return returnValue;
+        }
+
+        public String find(String input) {
+            return find(input.toCharArray());
+        }
+    }
+
+    private PrefixTrie prefixPrefixTrie = new PrefixTrie();
 
     public PrefixLookup(Map<String, String> prefixList) {
-        sortedPrefixList = new ArrayList<>(prefixList.size());
-
         // Translate the map into a different structure and lowercase the key.
-        prefixList.forEach((key, value) -> sortedPrefixList.add(Pair.of(key.toLowerCase(Locale.ENGLISH), value)));
-
-        // Sort the list to work best with the chosen algorithm.
-        // Sort values by length (longest first), then alphabetically.
-        sortedPrefixList.sort((o1, o2) -> {
-            String k1 = o1.getKey();
-            String k2 = o2.getKey();
-            int    l1 = k1.length();
-            int    l2 = k2.length();
-            if (l1 == l2) {
-                return k1.compareTo(k2);
-            }
-            return (l1 < l2) ? 1 : -1;
-        });
+        prefixList.forEach((key, value) -> prefixPrefixTrie.add(key.toLowerCase(Locale.ENGLISH), value));
     }
 
     public String findLongestMatchingPrefix(String input) {
-
-        String lowerInput = input.toLowerCase(Locale.ENGLISH);
-
-        // TODO: Experiment if a different datastructur is significantly faster (This implementation is SILLY simple)
-        for (Pair<String, String> prefix: sortedPrefixList) {
-            if (lowerInput.startsWith(prefix.getKey())) {
-                return prefix.getRight();
-            }
-        }
-        return null;
+        return prefixPrefixTrie.find(input.toLowerCase(Locale.ENGLISH));
     }
 
 }
--- analyzer/src/test/java/nl/basjes/parse/useragent/utils/TestPrefixLookup.java
+++ analyzer/src/test/java/nl/basjes/parse/useragent/utils/TestPrefixLookup.java
@@ -20,18 +20,22 @@ public class TestPrefixLookup {
         prefixMap.put("1",    "Result 1");
         prefixMap.put("12",   "Result 12");
         prefixMap.put("123",  "Result 123");
+        // The 1234 is missing !!!
+        prefixMap.put("12345",  "Result 12345");
 
         PrefixLookup prefixLookup = new PrefixLookup(prefixMap);
 
         assertNull(prefixLookup.findLongestMatchingPrefix("MisMatch"));
 
-        assertEquals("Result 1", prefixLookup.findLongestMatchingPrefix("1"));
-        assertEquals("Result 12", prefixLookup.findLongestMatchingPrefix("12"));
-        assertEquals("Result 123", prefixLookup.findLongestMatchingPrefix("123"));
+        assertEquals("Wrong result for '1'",             "Result 1",        prefixLookup.findLongestMatchingPrefix("1"));
+        assertEquals("Wrong result for '12'",            "Result 12",       prefixLookup.findLongestMatchingPrefix("12"));
+        assertEquals("Wrong result for '123'",           "Result 123",      prefixLookup.findLongestMatchingPrefix("123"));
+        assertEquals("Wrong result for '1234'",          "Result 123",      prefixLookup.findLongestMatchingPrefix("1234"));
+        assertEquals("Wrong result for '12345'",         "Result 12345",    prefixLookup.findLongestMatchingPrefix("12345"));
 
-        assertEquals("Result 123", prefixLookup.findLongestMatchingPrefix("1234"));
-        assertEquals("Result 12", prefixLookup.findLongestMatchingPrefix("12 Something"));
-        assertEquals("Result 1", prefixLookup.findLongestMatchingPrefix("1111"));
+        assertEquals("Wrong result for '1234'",          "Result 123",      prefixLookup.findLongestMatchingPrefix("1234"));
+        assertEquals("Wrong result for '12 Something'",  "Result 12",       prefixLookup.findLongestMatchingPrefix("12 Something"));
+        assertEquals("Wrong result for '1111'",          "Result 1",        prefixLookup.findLongestMatchingPrefix("1111"));
     }
 
     @Test
@@ -47,7 +51,7 @@ public class TestPrefixLookup {
 
         long start = System.nanoTime();
         for (int i = 0; i<iterations; i++) {
-            prefixLookup.findLongestMatchingPrefix("1");
+            prefixLookup.findLongestMatchingPrefix("999");
         }
         long stop = System.nanoTime();
         LOG.info("Speed stats: {} runs took {}ms --> {}us each.", iterations, (stop - start)/1000000, ((stop - start)/iterations)/1000);
