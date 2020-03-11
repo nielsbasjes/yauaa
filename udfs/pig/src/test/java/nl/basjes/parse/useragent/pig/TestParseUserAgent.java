065520938d607b87625584b9717f7ebd63789d9b
Niels Basjes <niels@basjes.nl>
2020-03-11 15:07:54 +0100

    Fix checkstyle omission

--- udfs/pig/src/test/java/nl/basjes/parse/useragent/pig/TestParseUserAgent.java
+++ udfs/pig/src/test/java/nl/basjes/parse/useragent/pig/TestParseUserAgent.java
@@ -30,7 +30,6 @@ import org.junit.jupiter.api.Test;
 import static org.apache.pig.builtin.mock.Storage.resetData;
 import static org.apache.pig.builtin.mock.Storage.tuple;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.fail;
