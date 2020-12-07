d60b3fbc72132544b6b1604a3db9a94e1eb44eaa
Niels Basjes <niels@basjes.nl>
2020-12-07 22:26:07 +0100

    Dependency updates

--- check-dependency-versions.sh
+++ check-dependency-versions.sh
@@ -4,6 +4,9 @@
 echo "==============================="
 echo "===- CHECKING DEPENDENCIES -==="
 echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
+echo "---------->"
+echo "----------> IGNORE UPDATE MESSAGES ABOUT com.esotericsoftware:kryo ... 4.0.2 -> 5.0.2"
+echo "---------->"
 mvn versions:display-dependency-updates versions:display-plugin-updates | grep -E '( -> | Building )'
 echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
 echo "===-         DONE!         -==="
