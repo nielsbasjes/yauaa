677c6ffcf06bef507ab876e6cb5961f3158ce1e4
Niels Basjes <niels@basjes.nl>
2021-04-14 12:24:59 +0200

    Update dependencies

--- check-dependency-versions.sh
+++ check-dependency-versions.sh
@@ -6,7 +6,7 @@ echo "===- CHECKING DEPENDENCIES -==="
 echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
 # NOTE: WE IGNORE UPDATE MESSAGES ABOUT com.esotericsoftware:kryo ... 4.0.2 -> 5.x.x
 #       These are needed to test for backwards compatibility when an older Kryo is used.
-mvn versions:display-dependency-updates versions:display-plugin-updates | grep -E '( -> | Building )' | \
+mvn -PpackageForRelease -PEnableReportPlugins versions:display-dependency-updates versions:display-plugin-updates | grep -E '( -> | Building )' | \
    grep -v 'com\.esotericsoftware:kryo .\+ 4.0.2 -> 5' | sort -u
 echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
 echo "===-         DONE!         -==="
