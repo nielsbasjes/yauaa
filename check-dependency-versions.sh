10452b1e74c74b1f716ee6ffdf641438d31b4997
Niels Basjes <niels@basjes.nl>
2021-04-29 22:30:03 +0200

    Improve check versions script

--- check-dependency-versions.sh
+++ check-dependency-versions.sh
@@ -6,8 +6,10 @@ echo "===- CHECKING DEPENDENCIES -==="
 echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
 # NOTE: WE IGNORE UPDATE MESSAGES ABOUT com.esotericsoftware:kryo ... 4.0.2 -> 5.x.x
 #       These are needed to test for backwards compatibility when an older Kryo is used.
-mvn -PpackageForRelease -PEnableReportPlugins versions:display-dependency-updates versions:display-plugin-updates | grep -E '( -> | Building )' | \
-   grep -v 'com\.esotericsoftware:kryo .\+ 4.0.2 -> 5'
+mvn -PpackageForRelease -PEnableReportPlugins versions:display-dependency-updates versions:display-plugin-updates | \
+  sed ':a;N;$!ba;s/\.\.\.\n\[INFO\] \+/ ... /g' | \
+  grep -E '( -> | Building )' | \
+  grep -v 'com\.esotericsoftware:kryo .\+ 4.0.2 -> 5'
 echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
 echo "===-         DONE!         -==="
 echo "==============================="
