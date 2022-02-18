08238ef30ec93c3dee20e386871a36c42b4e8878
Niels Basjes <niels@basjes.nl>
2022-02-18 14:41:41 +0100

    Build Quarkus example as part of normal build

--- examples/quarkus/src/test/java/nl/basjes/parse/useragent/quarkus/ParseServiceTest.java
+++ examples/quarkus/src/test/java/nl/basjes/parse/useragent/quarkus/ParseServiceTest.java
@@ -24,10 +24,10 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
-public class ParseServiceTest {
+class ParseServiceTest {
 
     @Test
-    public void testParserEndpoint() {
+    void testParserEndpoint() {
         given()
             .header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36")
             .when()
