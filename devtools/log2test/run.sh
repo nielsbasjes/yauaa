5867b5ec7d858d6ec724b1139ebd03200fb508dc
Niels Basjes <niels@basjes.nl>
2022-09-25 13:15:27 +0200

    docs: Minor cleanups

--- devtools/log2test/run.sh
+++ devtools/log2test/run.sh
@@ -21,5 +21,5 @@ JAR=$(ls target/yauaa-*.jar | fgrep -v source)
 
 FORMAT="%a %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\" \"%{Sec-CH-UA}i\" \"%{Sec-CH-UA-Arch}i\" \"%{Sec-CH-UA-Bitness}i\" \"%{Sec-CH-UA-Full-Version}i\" \"%{Sec-CH-UA-Full-Version-List}i\" \"%{Sec-CH-UA-Mobile}i\" \"%{Sec-CH-UA-Model}i\" \"%{Sec-CH-UA-Platform}i\" \"%{Sec-CH-UA-Platform-Version}i\" \"%{Sec-CH-UA-WoW64}i\" %V"
 
-java -jar "${JAR}" "${FORMAT}" __logs/access_clienthints_log*
+java -jar "${JAR}" "${FORMAT}" __logs/access_clienthints_log-20220925
 
