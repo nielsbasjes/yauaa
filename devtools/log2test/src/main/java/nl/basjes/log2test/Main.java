5a797f35aad6b87b4bc3aac6b691b4db4c936079
Niels Basjes <niels@basjes.nl>
2023-07-21 13:38:23 +0200

    test: Improve the log 2 test helper tool

--- devtools/log2test/src/main/java/nl/basjes/log2test/Main.java
+++ devtools/log2test/src/main/java/nl/basjes/log2test/Main.java
@@ -17,10 +17,6 @@
 
 package nl.basjes.log2test;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import nl.basjes.parse.core.Field;
 import nl.basjes.parse.core.Parser;
 import nl.basjes.parse.core.exceptions.DissectionFailure;
 import nl.basjes.parse.core.exceptions.InvalidDissectorException;
@@ -37,10 +33,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -52,146 +46,6 @@ public final class Main {
     private static final Logger LOG = LogManager.getLogger(Main.class);
 
     private Main() {
-    }
-
-    @EqualsAndHashCode
-    public static final class LogRecord implements Comparable<LogRecord>{
-        private static final String BASE = "HTTP.HEADER:request.header.";
-
-        @Getter @Setter(onMethod=@__(@Field("HTTP.USERAGENT:request.user-agent")))  private String userAgent              = null;
-        @Getter @Setter(onMethod=@__(@Field(BASE + "sec-ch-ua")))                   private String secChUa                = null;
-        @Getter @Setter(onMethod=@__(@Field(BASE + "sec-ch-ua-arch")))              private String secChUaArch            = null;
-        @Getter @Setter(onMethod=@__(@Field(BASE + "sec-ch-ua-bitness")))           private String secChUaBitness         = null;
-        @Getter @Setter(onMethod=@__(@Field(BASE + "sec-ch-ua-full-version")))      private String secChUaFullVersion     = null;
-        @Getter @Setter(onMethod=@__(@Field(BASE + "sec-ch-ua-full-version-list"))) private String secChUaFullVersionList = null;
-        @Getter @Setter(onMethod=@__(@Field(BASE + "sec-ch-ua-mobile")))            private String secChUaMobile          = null;
-        @Getter @Setter(onMethod=@__(@Field(BASE + "sec-ch-ua-model")))             private String secChUaModel           = null;
-        @Getter @Setter(onMethod=@__(@Field(BASE + "sec-ch-ua-platform")))          private String secChUaPlatform        = null;
-        @Getter @Setter(onMethod=@__(@Field(BASE + "sec-ch-ua-platform-version")))  private String secChUaPlatformVersion = null;
-        @Getter @Setter(onMethod=@__(@Field(BASE + "sec-ch-ua-wow64")))             private String secChUaWow64           = null;
-
-        private void putIfNotNull(Map<String, String> result, String header, String value) {
-            if (value != null) {
-                result.put(header, value);
-            }
-        }
-
-        public Map<String, String> asHeadersMap() {
-            Map<String, String> result = new TreeMap<>();
-            putIfNotNull(result, "User-Agent",                  userAgent);
-            putIfNotNull(result, "Sec-Ch-Ua",                   secChUa);
-            putIfNotNull(result, "Sec-Ch-Ua-Arch",              secChUaArch);
-            putIfNotNull(result, "Sec-Ch-Ua-Bitness",           secChUaBitness);
-            putIfNotNull(result, "Sec-Ch-Ua-Full-Version",      secChUaFullVersion);
-            putIfNotNull(result, "Sec-Ch-Ua-Full-Version-List", secChUaFullVersionList);
-            putIfNotNull(result, "Sec-Ch-Ua-Mobile",            secChUaMobile);
-            putIfNotNull(result, "Sec-Ch-Ua-Model",             secChUaModel);
-            putIfNotNull(result, "Sec-Ch-Ua-Platform",          secChUaPlatform);
-            putIfNotNull(result, "Sec-Ch-Ua-Platform-Version",  secChUaPlatformVersion);
-            putIfNotNull(result, "Sec-Ch-Ua-Wow64",             secChUaWow64);
-            return result;
-        }
-
-        private String escapeYaml(String input) {
-            return input.replace("'", "''");
-        }
-
-        private void appendIfNotNull(StringBuilder sb, String header, String value) {
-            if (value != null) {
-                sb.append("      ").append(header).append(" ".repeat(29-header.length())).append(": '").append(escapeYaml(value)).append("'\n");
-            }
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            sb.append("- test:\n");
-            sb.append("    input:\n");
-            appendIfNotNull(sb, "User-Agent",                  userAgent);
-            appendIfNotNull(sb, "Sec-Ch-Ua",                   secChUa);
-            appendIfNotNull(sb, "Sec-Ch-Ua-Arch",              secChUaArch);
-            appendIfNotNull(sb, "Sec-Ch-Ua-Bitness",           secChUaBitness);
-            appendIfNotNull(sb, "Sec-Ch-Ua-Full-Version",      secChUaFullVersion);
-            appendIfNotNull(sb, "Sec-Ch-Ua-Full-Version-List", secChUaFullVersionList);
-            appendIfNotNull(sb, "Sec-Ch-Ua-Mobile",            secChUaMobile);
-            appendIfNotNull(sb, "Sec-Ch-Ua-Model",             secChUaModel);
-            appendIfNotNull(sb, "Sec-Ch-Ua-Platform",          secChUaPlatform);
-            appendIfNotNull(sb, "Sec-Ch-Ua-Platform-Version",  secChUaPlatformVersion);
-            appendIfNotNull(sb, "Sec-Ch-Ua-Wow64",             secChUaWow64);
-            return sb.toString();
-        }
-
-        public int compareFields(String ours, String other) {
-            if (ours == null){
-                if (other == null) {
-                    return 0;
-                } else {
-                    return -1;
-                }
-            }
-            if (other == null) {
-                return 1;
-            }
-            return ours.compareTo(other);
-        }
-
-        @Override
-        public int compareTo(LogRecord other) {
-            int compare;
-
-            compare = compareFields(userAgent, other.userAgent);
-            if (compare != 0) {
-                return compare;
-            }
-
-            compare = compareFields(secChUa, other.secChUa);
-            if (compare != 0) {
-                return compare;
-            }
-
-            compare = compareFields(secChUaArch, other.secChUaArch);
-            if (compare != 0) {
-                return compare;
-            }
-
-            compare = compareFields(secChUaBitness, other.secChUaBitness);
-            if (compare != 0) {
-                return compare;
-            }
-
-            compare = compareFields(secChUaFullVersion, other.secChUaFullVersion);
-            if (compare != 0) {
-                return compare;
-            }
-
-            compare = compareFields(secChUaFullVersionList, other.secChUaFullVersionList);
-            if (compare != 0) {
-                return compare;
-            }
-
-            compare = compareFields(secChUaMobile, other.secChUaMobile);
-            if (compare != 0) {
-                return compare;
-            }
-
-            compare = compareFields(secChUaModel, other.secChUaModel);
-            if (compare != 0) {
-                return compare;
-            }
-
-            compare = compareFields(secChUaPlatform, other.secChUaPlatform);
-            if (compare != 0) {
-                return compare;
-            }
-
-            compare = compareFields(secChUaPlatformVersion, other.secChUaPlatformVersion);
-            if (compare != 0) {
-                return compare;
-            }
-
-            compare = compareFields(secChUaWow64, other.secChUaWow64);
-            return compare;
-        }
     }
 
     public static void printAllPossible(String logformat) {
