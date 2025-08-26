1cd677ddda1d58cdc4695a9a3b04a69c0f466298
Niels Basjes <niels@basjes.nl>
2025-08-26 11:37:18 +0200

    feat: Support for GraalVM Native compilation

---
+++ examples/graalvm-native/src/main/kotlin/nl/basjes/example/Main.kt
@@ -0,0 +1,26 @@
+/*
+ * Yet Another UserAgent Analyzer
+ * Copyright (C) 2013-2026 Niels Basjes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.basjes.example
+
+import nl.basjes.parse.useragent.UserAgentAnalyzer
+
+fun main() {
+    val analyzer = UserAgentAnalyzer.newBuilder().build()
+    val result = analyzer.parse("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36")
+    print(result)
+}
